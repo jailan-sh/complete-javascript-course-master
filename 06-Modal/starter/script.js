0e986f301627d5ff3f692c7af11d60d88f1ace95
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-31 02:16:03 +0200

    dom classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// spaacify selected classes
+
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openEvent = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeEvent = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openEvent);
+}
+
+btnCloseModal.addEventListener('click', closeEvent);
+overlay.addEventListener('click', closeEvent);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') closeEvent();
+});
