034efcf3691ae64595121aebaddc2a53eb8f954b
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-05 22:30:10 +0200

    challenge#1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -59,49 +59,121 @@ const restaurant = {
 // const [a, b, ...re] = ingredients;
 // console.log(a, b, re);
 
-const collect = function (...numbers) {
-  console.log(numbers);
-};
-collect(3, 5, 6, 7, 8, 9); // unknown arguments pass
+// const collect = function (...numbers) {
+//   console.log(numbers);
+// };
+// collect(3, 5, 6, 7, 8, 9); // unknown arguments pass
 
-const x = [3, 5, 6, 7, 8, 9];
-collect(...x); // spread operator
+// const x = [3, 5, 6, 7, 8, 9];
+// collect(...x); // spread operator
 
-restaurant.numGuests = 0; // edge case
-const guests = restaurant.numGuests || 1;
-console.log(guests); // 1 , 1
-// restaurant.numGuests = false;
-const guestsSolution = restaurant.numGuests ?? 1;
-console.log(guestsSolution); // 0, false
+// restaurant.numGuests = 0; // edge case
+// const guests = restaurant.numGuests || 1;
+// console.log(guests); // 1 , 1
+// // restaurant.numGuests = false;
+// const guestsSolution = restaurant.numGuests ?? 1;
+// console.log(guestsSolution); // 0, false
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+///////////////////////////////////////
+// Coding Challenge #1
 
 /* 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+We're building a football betting app (soccer for my American friends 😅)!
 
-The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-THIS TEST DATA (pasted to textarea)
-underscore_case
- first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
-underscoreCase      ✅
-firstName           ✅✅
-someVariable        ✅✅✅
-calculateAge        ✅✅✅✅
-delayedDeparture    ✅✅✅✅✅
-
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
-
-Afterwards, test with your own test data!
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 GOOD LUCK 😀
 */
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// coding:
+
+//1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4
+const players1Final = [...players1, 'Thiago', 'coutinho', 'perisic'];
+console.log(players1Final);
+
+//5
+/* const {
+  odds: { team1, x: draw, team2 },
+} = game;
+*/
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+//6
+const printGoals = function (...scored) {
+  console.log(`${scored.length} goals`);
+  console.log(...scored);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+//7
+const team1win = team1 < team2 && 'team1 is more likely to win';
+const team2win = team2 < team1 && 'team2 win is more likely to win';
+console.log(team2win || team1win);
