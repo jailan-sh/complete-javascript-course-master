fa2009a8e0e5f00412024eead4eeaf0fb269858b
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-06 22:02:14 +0200

    challenge#2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -97,6 +97,117 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// coding:
+
+// //1
+// const [players1, players2] = game.players;
+// console.log(players1);
+// console.log(players2);
+
+// //2
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// //3
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// //4
+// const players1Final = [...players1, 'Thiago', 'coutinho', 'perisic'];
+// console.log(players1Final);
+
+// //5
+// /* const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// */
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// //6
+// const printGoals = function (...scored) {
+//   console.log(`${scored.length} goals`);
+//   console.log(...scored);
+// };
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+// //7
+// const team1win = team1 < team2 && 'team1 is more likely to win';
+// const team2win = team2 < team1 && 'team2 win is more likely to win';
+// console.log(team2win || team1win);
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const [i, item] of menu.entries()) {
+//   console.log(i + 1 + ': ' + item);
+// }
+// console.log(...menu.entries());
+// for (const item of menu) console.log(item);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -137,43 +248,28 @@ const game = {
     team2: 6.5,
   },
 };
-
-// coding:
-
 //1
-const [players1, players2] = game.players;
-console.log(players1);
-console.log(players2);
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
 
 //2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+const result = Object.values(game.odds);
+let ave = 0;
+for (const item of result) {
+  ave += item;
+}
+console.log(ave / result.length);
 
 //3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-
-//4
-const players1Final = [...players1, 'Thiago', 'coutinho', 'perisic'];
-console.log(players1Final);
-
-//5
-/* const {
-  odds: { team1, x: draw, team2 },
-} = game;
-*/
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
-
-//6
-const printGoals = function (...scored) {
-  console.log(`${scored.length} goals`);
-  console.log(...scored);
-};
-
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
-//7
-const team1win = team1 < team2 && 'team1 is more likely to win';
-const team2win = team2 < team1 && 'team2 win is more likely to win';
-console.log(team2win || team1win);
+for (const [k, v] of Object.entries(game.odds)) {
+  const team = k === 'x' ? 'draw' : `victory ${game[k]}`;
+  console.log(`odd of ${team} ${v}`);
+  //console.log(`Odd of ${game[k] ?? 'draw'} : ${v}`);
+}
+//bonus  proparty : game.scored
+const scorers = {};
+for (const goal of game.scored) {
+  scorers[goal] ? scorers[goal]++ : (scorers[goal] = 1);
+}
+console.log(scorers);
