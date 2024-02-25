1e7acb058bd10bb16f11d169f6a6de728ac6acea
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-26 01:22:35 +0200

    oop

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,10 +1,18 @@
 'use strict';
 
-const Person = function (first, birthYear) {
-  this.first = first;
-  this.birthYear = birthYear;
-};
+// const Person = function (first, birthYear) {
+//   this.first = first;
+//   this.birthYear = birthYear;
+// };
 
-const jailan = new Person('jailan', 91);
+// const jailan = new Person('jailan', 91);
 
-console.log(jailan);
+// console.log(jailan);
+
+// const arr = [2, 2, 2, 3, 4, 4, 4];
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
