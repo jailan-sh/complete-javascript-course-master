4d6ceafcded4fcbcb9fc309644befc32dccaed16
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-05 01:38:44 +0200

    #23

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -185,35 +185,91 @@ GOOD LUCK 😀
 // console.log(car1);
 ///////////////////////////////////////////
 
-const Person = function (fullName, birthYear) {
-  this.fullName = fullName;
-  this.birthYear = birthYear;
-};
-// instence method method in prototype any created instance can use it
-Person.prototype.calAge = function () {
-  console.log(2034 - this.birthYear);
+// const Person = function (fullName, birthYear) {
+//   this.fullName = fullName;
+//   this.birthYear = birthYear;
+// };
+// // instence method method in prototype any created instance can use it
+// Person.prototype.calAge = function () {
+//   console.log(2034 - this.birthYear);
+// };
+
+// const Student = function (fullName, birthYear, course) {
+//   Person.call(this, fullName, birthYear);
+//   this.course = course;
+// };
+
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+// // special prototype for our object after inheritance
+// Student.prototype.interduce = function () {
+//   console.log(`my name is ${this.fullName} and courses are ${this.course}`);
+// };
+
+// const mike = new Student('mike', 2000, 'cs');
+// console.log(mike);
+// mike.calAge();
+// mike.interduce();
+// console.log(mike.__proto__);
+// console.log(mike.__proto__.__proto__);
+
+// console.log(mike instanceof Student);
+// console.log(mike instanceof Person);
+// console.log(mike instanceof Object);
+// console.dir(Student.prototype.constructor);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const Student = function (fullName, birthYear, course) {
-  Person.call(this, fullName, birthYear);
-  this.course = course;
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.speed} km/h`);
 };
 
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-// special prototype for our object after inheritance
-Student.prototype.interduce = function () {
-  console.log(`my name is ${this.fullName} and courses are ${this.course}`);
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.speed} km/h`);
+};
+// inheritance
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeBatter = function (chargeTo) {
+  this.charge = chargeTo;
 };
 
-const mike = new Student('mike', 2000, 'cs');
-console.log(mike);
-mike.calAge();
-mike.interduce();
-console.log(mike.__proto__);
-console.log(mike.__proto__.__proto__);
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
 
-console.log(mike instanceof Student);
-console.log(mike instanceof Person);
-console.log(mike instanceof Object);
-console.dir(Student.prototype.constructor);
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBatter(90);
+console.log(tesla);
+tesla.brake();
