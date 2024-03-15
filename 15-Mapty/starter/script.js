fb72e3191c91e00436dd1567e5fd86ced3cd8089
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-15 17:47:33 +0200

    leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -21,16 +21,27 @@ if (navigator.geolocation)
 
       //console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const map = L.map('map').setView(coords, 13);
-
+      // console.log(map);
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapevent) {
+        const { lat, lng } = mapevent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 250,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('jIaln')
+          .openPopup();
+      });
     },
     function () {
       alert(
