9af78e56d5b8dcf42082f2585020d386ac5ef690
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-16 17:54:08 +0200

    events

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,39 +13,66 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // geolocation API
 
+let map, mapevent;
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-      //console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      const map = L.map('map').setView(coords, 13);
-      // console.log(map);
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-      map.on('click', function (mapevent) {
-        const { lat, lng } = mapevent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 250,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('jIaln')
-          .openPopup();
-      });
-    },
-    function () {
-      alert(
-        'can not get your current location, Geolocation is not supported by this browser!....'
-      );
-    }
-  );
+    //console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    map = L.map('map').setView(coords, 13);
+    // console.log(map);
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on(
+      'click',
+      function (event) {
+        mapevent = event;
+        // display form
+
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      },
+      function () {
+        alert(
+          'can not get your current location, Geolocation is not supported by this browser!....'
+        );
+      }
+    );
+
+    // display form
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      inputDistance.value =
+        inputCadence.value =
+        inputDuration.value =
+        inputElevation.value =
+          '';
+      const { lat, lng } = mapevent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 250,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('workOut')
+        .openPopup();
+    });
+  });
+
+// toggle of input type
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
