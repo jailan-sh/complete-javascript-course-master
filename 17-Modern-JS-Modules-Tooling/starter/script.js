28a3f9c8f83f16b479b7a1ef4667a2920c887fc1
jailan-sh <jilan.elsherbiny@gmail.com>
2024-03-30 01:09:45 +0200

    up

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,3 @@
+console.log('like');
+import './shoppingCart';
+17-Modern-JS-Modules-Tooling/starter/script.js
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,2 @@
+console.log('exportind Module');
+C:\Users\Acadmya\Downloads\complete-javascript-course-master\17-Modern-JS-Modules-Tooling\starter\shoppingCart.js
